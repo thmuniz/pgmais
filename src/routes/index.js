8bc209e8bae7df536c6873bde3ef774c589ceba6
Thiago <thmuniz93@gmail.com>
2019-05-01 17:27:04 -0300

    indentation

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,12 @@ export default function execute(app){
     app.use(function(req, res, next) {
         req.getUrl = function() {
             return req.protocol + "://" + req.get('host') + req.originalUrl
-        };
+        }
         req.getSimpleUrl = function() {
             return req.protocol + "://" + req.get('host')
-        };
+        }
         return next()
-    });
+    })
 
     // Define todas as rotas que serão utilizadas
         app.use('/users',
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,40 +4,46 @@ import errorHandler from '../helpers/errorHandler'
 import User from '../models/user'
 import fs  from 'fs' 
 import multer from 'multer'
-import fastCsv  from 'fast-csv';
-import { cleanFileData } from '../helpers/fileHelper';
+import fastCsv  from 'fast-csv'
+import path from 'path'
+import { cleanFileData } from '../helpers/fileHelper'
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
       cb(null, 'tmp/csv/')
   },
   filename: function (req, file, cb) {
-    console.log(file)
-      cb(null, `${file.originalname}`);
+      cb(null, `${file.originalname}`)
   }
-});
-const upload = multer({ storage });
+})
+const upload = multer({ storage })
 
 /**
  * Teste
  */
 router.post('/', upload.single('file'),  async (req, res) => {
     try {
-		const rows = [];
-        // open uploaded file
+		const rows = []
         fastCsv.fromPath(req.file.path)
 		.on("data", function (eachRow) {
-            rows.push(eachRow);
-          })
-          .on("end", async function () {
-            let clean = await cleanFileData(rows);
-            console.log(clean)
-           
-            //fs.unlinkSync(req.file.path);   // remove temp file
-            //process "fileRows" and respond
-          })
+            rows.push(eachRow)
+		})
+			.on("end", async function () {
+				let clean = await cleanFileData(rows)
+				return res.status(200).json(clean)
+			})
+				.on("error", function() {
+					fs.unlinkSync(req.file.path)
+					if(path.extname(req.file.originalname) !== ".csv") {
+						return errorHandler.handleError({
+							error: {status: 400, error: `Invalid file extension.`, display: true},
+							res: res,
+							req: req
+						})
+					}
+				})
     } catch (e) {
-        errorHandler.logger.log("error", e);
+        errorHandler.logger.log("error", e)
         return errorHandler.handleError({
             error: {status: 500, error: `Error: ${e}`, display: false},
             res: res,
@@ -46,4 +52,4 @@ router.post('/', upload.single('file'),  async (req, res) => {
     } 
 })
 
-export default router;
+export default router
